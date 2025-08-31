a6fc184d3747a8796601134f0c7b7292a4b306fb
Michaelshenmo <121526475+Michaelshenmo@users.noreply.github.com>
2025-08-31 14:51:29 +0800

    1.1 Commit

--- CancelPlannedPowerPlan.py
+++ CancelPlannedPowerPlan.py
@@ -1,9 +1,16 @@
 import os
+import subprocess
+import winsound
 
-returnvalue = os.system('shutdown /a')
+sound_path_back = os.path.expandvars('%SystemRoot%\\Media\\Windows Background.wav')
+sound_path_fore = os.path.expandvars('%SystemRoot%\\Media\\Windows Foreground.wav')
+returnvalue = subprocess.call('shutdown /a', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 if returnvalue == 1116:
-    os.system('start C:\\AutoShutdownPlanner\\NoPlan.vbs')
+    os.system('start /B .\MessageBox.exe 因为没有任何进行中的关机过程，所以无法中止系统关机。(1116) 错误 0 16')
+    winsound.PlaySound(sound_path_fore, winsound.SND_FILENAME)
 elif returnvalue == 0:
-    os.system('start C:\\AutoShutdownPlanner\\Success.vbs')
+    os.system('start /B .\MessageBox.exe 计划的电源计划已被取消。(0) 成功 0 80')
+    winsound.PlaySound(sound_path_back, winsound.SND_FILENAME)
 else:
-    os.system('start C:\\AutoShutdownPlanner\\Failed.vbs')
+    os.system('start /B .\MessageBox.exe 程序遇到未知错误。(' + str(returnvalue) + ') 错误 0 16')
+    winsound.PlaySound(sound_path_fore, winsound.SND_FILENAME)
